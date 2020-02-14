6763bbe3b3212f875b7396cb5a96b788ac19e673
georges@wow4results.co.za <georges@wow4results.co.za>
2020-02-14 14:24:50 +0200

    initiated auth feature to user with jwt

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,9 @@
+const config = require('./../config/config').get(process.env.NODE_ENV);
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('brcrypt');
+const SALT_I = 10;
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -25,6 +30,65 @@ const userSchema = new Schema({
 }, {
   timestamps: true,
 });
+
+userSchema.pre('save', function(next){
+  var user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(SALT_I, function(err, salt){
+      if(err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err) return next(err);
+
+        user.password = hash;
+        next();
+      })
+    })
+  }
+    else {
+      next();
+  }
+})
+
+userSchema.methods.comparePassword = function(canditatePassword, cb){
+  bcrypt.compare(canditatePassword, this.password, function(err, isMatch){
+    if(err) return cb(err);
+
+    cb(null, isMatch);
+  })
+}
+
+userSchema.methods.genToken = function(cb){
+  var user = this;
+  const token = jwt.sign(user._id.toHexString(), config.SECRET);
+
+  user.token = token;
+  user.save(function(err, user){
+    if(err) return cb(err);
+    cb(null, user);
+  })
+
+}
+
+userSchema.statics.findByToken = function(token, cb){
+  var user = this;
+  jwt.verify(token, config.SECRET, function(err, decode){
+    user.findOne({"_id":decode, "token":token}, function(err, user){
+      if(err) return cb(err);
+      cb(null, user);
+    })
+  })
+}
+
+userSchema.methods.deleteToken = function(token, cb){
+  var user = this;
+
+  user.update({$unset:{token:1}}, function(err, user){
+    if(err) return cb(err);
+    cb(null, user);
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
